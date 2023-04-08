69ec5cfe3b0242467d725cfdcc421d50beb82597
Gibsongf <maccgiiibson@gmail.com>
2023-04-08 20:16:03 -0300

    item selected pop-up  btns buy & quantity

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
-
+import "./App.css";
+import { Items, ItemSelected } from "./components/Items";
+import Img from "../src/imgs/tactics/Alchemic_Carbine_Icon.png";
+import { useState } from "react";
 function App() {
-  return (
-    <div className="App">
+	const d = `A crossbow-type ranged weapon which poisons enemies hit by its bolts`;
+	const [quantity, setQuantity] = useState(0);
+
+	return (
+		<div className="App">
+			<ItemSelected
+				name={"Alchemic Carbine"}
+				imgSrc={Img}
+				price={100}
+				description={d}
+        val={quantity}
+        setVal={setQuantity}
+			/>
       
-    </div>
-  );
+		</div>
+	);
 }
 
 export default App;
--- src/components/Items.js
+++ src/components/Items.js
@@ -1,22 +1,74 @@
-const Items = (props) => {
-	const { name, description, img, price } = props;
+import { useState } from "react";
+import "../components/Item.css";
+export const Items = (props) => {
+	const { name, description, imgSrc, price } = props;
+	const [quantity, setQuantity] = useState(0);
+	const divClicked = () => {
+		return;
+	};
 	// ?btn = add direct to the cart a props.addItem
 	// div.click => itemClicked()
 	return (
-		<div className="item" >
-			<img src={img.src} alt={name} className="item-img" />
+		<div className="item">
+			<img src={imgSrc} alt={name} className="item-img" />
 			<h4>{name}</h4>
 			<p>${price}</p>
+			{}
 		</div>
 	);
 };
-export default Items
-// itemClicked () =>
-//      pop-up that shows
-//      Img description
-//      btn type range = quantity regulator
-//      btn add to cart
-//      btn close pop-up
-//<p>{description}</p>
-//<button type="range"></button>
-// }
+const Btns = (props) => {
+	const { val, handler, addToCart } = props.states;
+	const changeQuantity = (e) => {
+		handler(e.target.value);
+	};
+	return (
+		<span>
+			<input
+				type="number"
+				min="1"
+				max="100"
+				step="1"
+				value={val}
+				onChange={changeQuantity}
+			/>
+			{/* <button onClick={addToCart} className="addToCart">
+				Add To Cart
+			</button> */}
+		</span>
+	);
+};
+export const ItemSelected = (props) => {
+	const { name, description, imgSrc, price, val, setVal } = props;
+	const changeQuantity = (e) => {
+		setVal(e.target.value);
+	};
+	return (
+		<div id="pop-up">
+			<div id="pop-up-content">
+				<button className="close-window">X</button>
+				<img src={imgSrc} alt={name} className="item-img-select" />
+				<div className="item-info">
+					<h2>{name}</h2>
+					<p className="description">{description}</p>
+					<p>${price}</p>
+				</div>
+				<span>
+					<label htmlFor="quantity">
+						Quantity:
+						<input
+                            id="quantity"
+							type="number"
+							min="1"
+							max="100"
+							step="1"
+							value={val}
+							onChange={changeQuantity}
+						/>
+					</label>
+					<button className="addToCart">Add To Cart</button>
+				</span>
+			</div>
+		</div>
+	);
+};
